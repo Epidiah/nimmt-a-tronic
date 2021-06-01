97bdb9608fb6f0dbe60d63c58cdf642f31d8a245
Epidiah <Epidiah@gmail.com>
2021-06-01 09:45:30 -0400

    I think it's working now.

--- nimmt.py
+++ nimmt.py
@@ -2,6 +2,7 @@
 
 from random import shuffle
 
+
 def construct_deck():
     """
     Assembles the cards because I'm not paid enough for data entry.
@@ -98,17 +99,21 @@ def nickname(card):
             "neufy-neuf",
         ][card["number"] % 11]
 
-class game():
-    def __init__(self, players):
+
+class game:
+    def __init__(self, *players):
         if not (2 <= len(players) <= 10):
-            raise ValueError('Only works with 2 to 10 players.')
+            raise ValueError("Only works with 2 to 10 players.")
         self.players = players
-        for player in players:
+        self.reset()
+
+    def reset(self):
+        for player in self.players:
             player.score = 66
-        self.deck = construct_deck()
-        self.table = [[],[],[],[]]
-    
+        self.table = [[], [], [], []]
+
     def deal(self):
+        self.deck = construct_deck()
         shuffle(self.deck)
         for player in self.players:
             player.hand = []
@@ -117,43 +122,51 @@ class game():
         for n in range(4):
             self.table[n].clear()
             self.table[n].append(self.deck.pop())
+        self.high_cards = [row[-1]["number"] for row in self.table]
 
     def score_row(self, row):
-        return sum([ card['heads'] for card in self.table[row] ])
+        return sum([card["heads"] for card in self.table[row]])
 
     def place_card(self, card, player):
-        high_cards = [ row[-1]['number'] for row in self.table ]
-        if card['number'] < min(high_cards):
-            row = player.row_strategy(self)
+        if card["number"] < min(self.high_cards):
+            row = player.row_strategy(player, self)
             player.score -= self.score_row(row)
             self.table[row].clear()
             self.table[row].append(card)
         else:
-            distances = [card['number'] - hc for hc in high_cards]
+            distances = [card["number"] - hc for hc in self.high_cards]
             closest = distances.index(min(distances))
-            if self.table[closest] > 6:
+            if len(self.table[closest]) >= 5:
                 player.score -= self.score_row(closest)
                 self.table[closest].clear()
             self.table[closest].append(card)
+        self.high_cards = [row[-1]["number"] for row in self.table]
 
     def play_turn(self):
-        self.cards_played = [ (player.card_strategy(self), player) for player in self.players ].sort(reverse=True)
+        self.cards_played = sorted([
+            (player.card_strategy(player, self), player) for player in self.players
+        ], key=lambda x: x[0]['number'], reverse=True)
         while self.cards_played:
             card, player = self.cards_played.pop()
             self.place_card(card, player)
             player.hand.pop(player.hand.index(card))
 
     def play(self):
-        turns = 0
+        hands = 0
         while min([player.score for player in self.players]) > 0:
-            turns += 1
+            hands += 1
+            self.deal()
             for n in range(10):
-                self.play_turn
-        return turns, [player.name, player.score for player in self.players].sort(key=lambda x: x[1], reverse=True)
+                self.play_turn()
+            print(
+                f"{hands}: {[(player.name, player.score) for player in self.players]}"
+            )
+        return hands, sorted([(player.name, player.score) for player in self.players],
+            key=lambda x: x[1], reverse=True
+        )
 
 
-
-class player():
+class player:
     def __init__(self, name, card_strategy, row_strategy):
         self.name = name
         self.card_strategy = card_strategy
@@ -168,13 +181,48 @@ class player():
     def __str__(self):
         return self.name
 
+
 def basic_card_strategy(player, game):
-    # First check for a safe play
-
-    # Then check for a risky brisket
-
+    hand = sorted(player.hand, key=lambda c: c["number"], reverse=True)
+    # First check for a safe play and play the highest safe play.
+    slots = [5 - len(row) for row in game.table]
+    for card in hand:
+        for row in range(4):
+            if (
+                game.high_cards[row]
+                < card["number"]
+                <= game.high_cards[row] + slots[row]
+            ):
+                return card
+    # Then check for least risky brisket and play highest of those
+    play = None
+    risk = 1
+    for card in hand:
+        for row in range(4):
+            if (card["number"] - (game.high_cards[row] + 2 * slots[row])) < risk:
+                risk = card["number"] - (game.high_cards[row] + 2 * slots[row])
+                play = card
+        if play:
+            return play
     # Finally, plays lowest card
-    pass
+    return hand[-1]
+
 
 def basic_row_strategy(player, game):
-    pass
+    chosen_row = 5
+    damage = 28
+    for row in range(len(game.table)):
+        row_total = sum([card["heads"] for card in game.table[row]])
+        if row_total < damage:
+            damage = row_total
+            chosen_row = row
+    return chosen_row
+
+
+# Testing
+a = player("a", basic_card_strategy, basic_row_strategy)
+b = player("b", basic_card_strategy, basic_row_strategy)
+c = player("c", basic_card_strategy, basic_row_strategy)
+d = player("d", basic_card_strategy, basic_row_strategy)
+
+g = game(a, b, c, d)
